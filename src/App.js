618c46326a6fc0b77401d5d58f420fd8df389453
HungHCC <buisihung321@gmail.com>
2021-07-05 22:27:16 +0700

    Add react slick

--- src/App.js
+++ src/App.js
@@ -7,16 +7,60 @@ import {
   VStack,
   Code,
   Grid,
-  theme,
+  extendTheme,
+  Image,
 } from '@chakra-ui/react';
 import Navigation from './components/Nav.jsx';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 import { Logo } from './Logo';
+import banner1 from './assets/images/banner1.png';
+import Slider from 'react-slick';
 
+const theme = extendTheme({
+  colors: {
+    primary: '#E92529',
+  },
+});
+
+const settings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+};
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Navigation  />
+      <Box h="100vh">
+        <Navigation />
+        <Box>
+          <Slider {...settings}>
+            <Image
+              w="100vw"
+              h="100vh"
+              objectFit="cover"
+              src={banner1}
+              alt="Segun Adebayo"
+            />
+            <Image
+              w="100vw"
+              h="100vh"
+              objectFit="cover"
+              src={banner1}
+              alt="Segun Adebayo"
+            />
+            <Image
+              w="100vw"
+              h="100vh"
+              objectFit="cover"
+              src={banner1}
+              alt="Segun Adebayo"
+            />
+          </Slider>
+        </Box>
+      </Box>
+
       <Box textAlign="center" fontSize="xl">
         <Grid minH="100vh" p={3}>
           <ColorModeSwitcher justifySelf="flex-end" />
